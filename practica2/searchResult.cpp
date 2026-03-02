2f48b227fbc71275967943ebd2a486409d242367
alballr <alba.llorente@alu.uclm.es>
2026-03-02 18:51:24 +0100

    implementacion del main

---
+++ practica2/searchResult.cpp
@@ -0,0 +1,4 @@
+#include <searchResult.h>
+
+SearchResult::SearchResult(int line, int previous, int next )
+    : Line(line), Previous(previous), Next(next) {}
---
+++ practica2/searchResult.h
@@ -0,0 +1,9 @@
+
+class SearchResult {
+    private:
+        int Line;
+        int Previous;
+        int Next;
+    public:
+        SearchResult(int line, int previous, int next);
+};
